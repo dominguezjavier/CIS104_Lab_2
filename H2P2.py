ee5072aeb56768d7a432ad97102a123351ee37c1
dominguezjavier <46700150+dominguezjavier@users.noreply.github.com>
2019-02-12 18:28:54 -0800

    Add files via upload
    I submitted before and it was done before today's date of 2/12/2019

---
+++ H2P2.py
@@ -0,0 +1,3 @@
+degree = int(input("What is the current temperature in degrees today?" ))
+degrees_to_celsius = (degree - 32 * 5/9)
+print("It is currently " + str(degree) + " degrees. However it is " + str(degrees_to_celsius) + " celsius today")
